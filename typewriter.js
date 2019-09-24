32a72f4ab903cf661bbcae288ae4894c8d03f857
K1 <k.kholghi@gmail.com>
2019-09-24 19:51:47 +0000

    Revised animatedSentence function for the inteval

--- typewriter.js
+++ typewriter.js
@@ -1,10 +1,11 @@
 const sentence = "hello there from lighthouse labs";
-
+let time = 1000;
 const animatedSentence = function (sentence) {
     for (const char of sentence) {
         setTimeout(() => {
-            process.stdout.write(`\n${char}`)
-        }, 50);
+            process.stdout.write(`${char}`)
+        }, time);
+        time += 50;
     }
 
 }

b347545b22e0bcb581bb2c686192d982efa2b4bf
K1 <k.kholghi@gmail.com>
2019-09-24 18:04:43 +0000

    Added a function to my typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -1,6 +1,13 @@
 const sentence = "hello there from lighthouse labs";
-for (const char of sentence) {
-    setTimeout(() => {
-        process.stdout.write(`\n${char}`)
-    }, 50);
+
+const animatedSentence = function (sentence) {
+    for (const char of sentence) {
+        setTimeout(() => {
+            process.stdout.write(`\n${char}`)
+        }, 50);
+    }
+
 }
+
+
+animatedSentence(sentence)
